cd111f976c6d26d0244b277fd68635370ee7262a
lmd59 <laurend@mitre.org>
2023-02-23 16:20:08 -0500

    custom headers and data_requirements_utils updates

--- lib/measure_repository_service_test_kit/library_data_requirements.rb
+++ lib/measure_repository_service_test_kit/library_data_requirements.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'json'
-require_relative '../utils/general_utils'
+require_relative '../utils/data_requirements_utils'
 
 module MeasureRepositoryServiceTestKit
   # tests for Library $package service
@@ -12,12 +12,15 @@ module MeasureRepositoryServiceTestKit
     title 'Library $data-requirements'
     description 'Ensure measure repository service can execute the $data-requirements operation to the Library endpoint'
     id 'library_data_requirements'
+    custom_headers = { 'content-type': 'application/fhir+json' }
 
     fhir_client do
       url :url
+      headers custom_headers
     end
 
     test do
+      include DataRequirementsUtils
       title '200 response and JSON Library body for POST by id in url'
       id 'library-data-requirements-01'
       description 'returned response has status code 200 and valid JSON FHIR Library with data requirement in body'
@@ -25,16 +28,12 @@ module MeasureRepositoryServiceTestKit
       makes_request :library_data_requirements
       run do
         fhir_operation("Library/#{library_id}/$data-requirements", name: :library_data_requirements)
-        assert_response_status(200)
-        assert_resource_type(:library)
-        assert_valid_json(response[:body])
-        assert(resource.dataRequirement, "No data requirement created for library with id: #{library_id}")
-        assert resource.type.coding[0].code == 'module-definition',
-               'Resulting data requirements Library is not type module-definition'
+        assert_dr_success
       end
     end
 
     test do
+      include DataRequirementsUtils
       title '200 response and JSON Library body for POST with url in body'
       id 'library-data-requirements-02'
       description 'returned resopnse has status code 200.'
@@ -52,16 +51,12 @@ module MeasureRepositoryServiceTestKit
         params = FHIR::Parameters.new params_hash
 
         fhir_operation('Library/$data-requirements', body: params)
-        assert_response_status(200)
-        assert_resource_type(:library)
-        assert_valid_json(response[:body])
-        assert(resource.dataRequirement, "No data requirement created for library with url: #{library_url}")
-        assert resource.type.coding[0].code == 'module-definition',
-               'Resulting data requirements Library is not type module-definition'
+        assert_dr_success
       end
     end
 
     test do
+      include DataRequirementsUtils
       title '200 response and JSON Library body for POST with identifier in body'
       id 'library-data-requirements-03'
       description 'returned response has status code 200.'
@@ -79,17 +74,12 @@ module MeasureRepositoryServiceTestKit
         params = FHIR::Parameters.new params_hash
 
         fhir_operation('Library/$data-requirements', body: params)
-        assert_response_status(200)
-        assert_resource_type(:library)
-        assert_valid_json(response[:body])
-        assert(resource.dataRequirement,
-               "No data requirement created for library with identifier: #{library_identifier}")
-        assert resource.type.coding[0].code == 'module-definition',
-               'Resulting data requirements Library is not type module-definition'
+        assert_dr_success
       end
     end
 
     test do
+      include DataRequirementsUtils
       title '200 response and JSON Bundle body for POST parameters url, identifier, and version in body and id in url'
       id 'library-data-requirements-04'
       description 'returned repsonse has status code 200.'
@@ -110,44 +100,36 @@ module MeasureRepositoryServiceTestKit
         params = FHIR::Parameters.new params_hash.freeze
 
         fhir_operation("Library/#{library_id}/$data-requirements", body: params)
-        assert_response_status(200)
-        assert_resource_type(:library)
-        assert_valid_json(response[:body])
-        assert(resource.dataRequirement, "No data requirement created for library with id: #{library_id}")
-        assert resource.type.coding[0].code == 'module-definition',
-               'Resulting data requirements Library is not type module-definition'
+        assert_dr_success
       end
     end
 
     test do
+      include DataRequirementsUtils
       title 'Throws 404 when no Library on server matches id'
       id 'library-data-requirements-05'
       description 'returns 404 status code with OperationOutcome when no Library exists with passed-in id'
 
       run do
         fhir_operation('Library/INVALID_ID/$data-requirements')
-        assert_response_status(404)
-        assert_valid_json(response[:body])
-        assert(resource.resourceType == 'OperationOutcome')
-        assert(resource.issue[0].severity == 'error')
+        assert_dr_failure(expected_status: 404)
       end
     end
 
     test do
+      include DataRequirementsUtils
       title 'Throws 400 when no id, url, or identifier provided'
       id 'library-data-requirements-06'
       description 'returns 400 status code with OperationOutcome when no id, url, or identifier provided'
 
       run do
         fhir_operation('Library/$data-requirements')
-        assert_response_status(400)
-        assert_valid_json(response[:body])
-        assert(resource.resourceType == 'OperationOutcome')
-        assert(resource.issue[0].severity == 'error')
+        assert_dr_failure
       end
     end
 
     test do
+      include DataRequirementsUtils
       title 'Throws 400 when id is included in both the path and as a FHIR parameter'
       id 'library-data-requirements-07'
       description 'returns 400 status code with OperationOutcome when no id, url, or identifier provided'
@@ -163,10 +145,7 @@ module MeasureRepositoryServiceTestKit
 
         params = FHIR::Parameters.new params_hash
         fhir_operation("Library/#{library_id}/$data-requirements", body: params)
-        assert_response_status(400)
-        assert_valid_json(response[:body])
-        assert(resource.resourceType == 'OperationOutcome')
-        assert(resource.issue[0].severity == 'error')
+        assert_dr_failure
       end
     end
   end
